5e784490a2a62c4df7a43c438de5ad68ce1e70d9
sina <sinakabki@gmail.com>
2019-01-16 19:41:27 +0000

    error handling set up

--- api/src/routes/api.js
+++ api/src/routes/api.js
@@ -4,23 +4,25 @@ const request = require('request');
 // got blocked by cloudflare, cloudscraper allows us to bypass the waf.
 const cloudScraper = require('cloudscraper');
 
-router.get('/email/:account', (req, res)  => {
+router.get('/email/:account', (req, res, next)  => {
     cloudScraper.get(`https://api.haveibeenpwned.com/unifiedsearch/${req.params.account}`, (error, response, body) => {
       if (error) {
         res.send(error);
+        next(error);
       }
       res.json(body);
     });
 });
 
-router.get('/password/:hash', (req, res)  => {
+router.get('/password/:hash', (req, res, next)  => {
   request(`https://api.pwnedpasswords.com/range/${req.params.hash}`, (error, response, body) => {
     if (error) {
       res.send(error);
+      next(error);
     }
     res.json(body);
   });
 });
 
 
-module.exports = router;
+module.exports = router;  
--- api/src/server.js
+++ api/src/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use('/api', routes);
 
+// error handler
+app.use((err, req, res, next) => {
+  res.status(500).send({error: err.message});
+});
 
 app.listen(4000, () => {
   console.log('Project set up');
