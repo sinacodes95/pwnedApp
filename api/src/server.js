897d2f5dfa6c2767c45bc636c50b91d13e858850
sina <sinakabki@gmail.com>
2019-01-16 18:55:48 +0000

    server.js upadated and required packages installed

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const routes = require('./routes/api')
 
 const app = express();
 
 app.use(cors());
+app.use('/api', routes);
+
 
 app.listen(4000, () => {
-  console.log('Project set up')
-})
+  console.log('Project set up');
+});
