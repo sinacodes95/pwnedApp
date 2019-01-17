3afdf3c23681cfecb91056f5248d39b16a89bd99
sina <sinakabki@gmail.com>
2019-01-17 01:19:17 +0000

    react-bootstrap set up and header implemented

---
+++ web/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+        <header className="header">
+          <h1 className="h1style"> HAVE YOU BEEN PAWNED? </h1>
+        </header>
+      </div>
+  );
+}
+
+export default Header;
--- web/src/containers/App.js
+++ web/src/containers/App.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from '../components/Header'
+import { Button } from 'react-bootstrap';
+
+const getBreachedEmails = () => {
+  fetch(`http://localhost:4000/api/email/test`)
+    .then(res => res.json())
+    .then(res => console.log(JSON.parse(res)))
+    .catch(err => console.log(err))
+}
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      email: {
+        emailInput: '',
+        emailOutput: [],
+        isEmailActive: false
+      },
+      password: {
+        passwordInput: '',
+        passwordOutput: []
+      }
+
+
+    }
+  }
+  componentDidMount() {
+    // getBreachedEmails()
+  }
+
   render() {
+    let { isEmailActive } = this.state.email;
+    
     return (
       <div className="App">
-        <h1></h1>
+        <Header />
+
+
+
+
       </div>
     );
   }
