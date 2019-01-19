68870314ef9f434d818ebc1be95a16c4dd8ba50c
sina <sinakabki@gmail.com>
2019-01-19 21:59:35 +0000

    Password component functionality implemented

--- web/src/containers/Email.js
+++ web/src/containers/Email.js
@@ -5,44 +5,127 @@ class Email extends Component {
     super()
     this.state = {
 
+      emailInput: '',
+      emailOutput: [],
+      emailNotFound: ''
+
     }
   }
+
   render() {
+    const { emailOutput, emailNotFound } = this.state;
     return (
       <div className='container'>
-        <div class="row">
-          <div class="col s12 m6">
-            <div class="card grey darken-1">
-              <div class="card-content white-text">
-                <span class="card-title">Email</span>
+        <div className="row">
+          <div className="col s12 m4">
+            <div className="card grey darken-1">
+              <div className="card-content white-text">
+                <span className="card-title">Email</span>
                 <p>Enter your email address below to check if it was compromised in a data breach.</p>
                 <p>You may also enter a list of comma separated emails to check multiple accounts.</p>
                 <br></br>
                 <br></br>
-                <div class="row">
-                  <form class="col s12">
-                    <div class="row">
-                      <div class="input-field col s11 m11">
-                        <i class="material-icons prefix">mail</i>
-                        <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
-                        <label for="icon_prefix2">Enter email</label>
+                <div className="row">
+                  <form className="col s12">
+                    <div className="row">
+                      <div className="input-field col s11 m11">
+                        <i className="material-icons prefix">mail</i>
+                        <textarea onChange={this.emailInput} id="icon_prefix2" className="materialize-textarea"></textarea>
+                        <label htmlFor="icon_prefix2">Enter email</label>
                       </div>
                     </div>
                   </form>
                 </div>
               </div>
-              <div class="card-action">
-                <a href="#">search</a>
+              <div className="card-action">
+                <a onClick={this.getBreachedEmails} href="#">search</a>
               </div>
             </div>
           </div>
+          <div className="col s12 m8">
+            {
+              emailOutput.length?emailOutput.map((emailBreach, index) => {
+                return (
+                  <div key={index} className='white-text'>
+                    <h3>{emailBreach.email}</h3>
+                    <table>
+                      <thead>
+                        <tr className='grey darken-1'>
+                          <th>Name</th>
+                          <th>Domain</th>
+                          <th>Breach Date</th>
+                          <th>Pwnd Count</th>
+                        </tr>
+                      </thead>
+                      {
+                        emailBreach.Breaches.map((breach, index) => {
+                          return (
+                          <tbody key={index}>
+                            <tr>
+                              <td>{breach.Name}</td>
+                              <td>{breach.Domain}</td>
+                              <td>{breach.BreachDate}</td>
+                              <td>{breach.PwnCount}</td>
+                            </tr>
+                          </tbody>
+                          );
+                        })
+                      }
+                    </table>
+                  </div>
+                )
+              })
+              :null}
+              {emailNotFound.length?emailNotFound.split(',').map((email,index) => {
+                return (
+                  email.length>5?<div key={index} className='white-text'>
+                      <h5 className='grey darken-1'>Wohooo! no records found for:</h5>
+                      <p>{email}<i className="material-icons prefix">check_circle</i></p>
+                  </div>:null
+                );
+              })
+              :null}
+          </div>
         </div>
-
-
-
       </div>
     );
   }
+
+  emailInput = (event) => {
+    this.setState({ emailInput: event.target.value });
+  }
+
+  getBreachedEmails = () => {
+    this.setState({ emailOutput: [], emailNotFound: '' });
+
+    let emails = this.state.emailInput;
+    emails = emails.replace(/\s/g, '')
+    emails = emails.match(/,/g) ? emails.split(',') : [emails];
+
+    emails.forEach(element => {
+      if (emails.length) {
+        fetch(`http://localhost:4000/api/email/${element}`)
+          .then(res => {
+            console.log(res)
+            return res
+          })
+          .then(res => res.json())
+          .then(res => JSON.parse(res))
+          .then(res => {
+            res['email'] = element;
+            return res;
+          })
+          .then(res => this.setState({ emailOutput: [...this.state.emailOutput, res] }))
+          .catch(err => {
+            if (err) {
+              console.log(err)
+              this.setState({ emailNotFound: `${this.state.emailNotFound},${element}` });
+            }
+          })
+      }
+    });
+  }
+
 }
 
 export default Email;
--- web/src/containers/Password.js
+++ web/src/containers/Password.js
@@ -4,43 +4,84 @@ class Password extends Component {
   constructor() {
     super()
     this.state = {
-
+      passwordInput: '',
+      passwordOutput: '',
+      isPasswordActive: false,
+      passwordNotFound: false
     }
   }
+
   render() {
     return (
       <div className='container'>
-        <div class="row">
-          <div class="col s12 m6">
-            <div class="card blue-grey darken-1">
-              <div class="card-content white-text">
-                <span class="card-title">Password</span>
+        <div className="row">
+          <div className="col s12 m6">
+            <div className="card blue-grey darken-1">
+              <div className="card-content white-text">
+                <span className="card-title">Password</span>
                 <p>Enter your password below to check if it was compromised in a data breach.</p>
                 <br></br>
                 <br></br>
-                <div class="row">
-                  <form class="col s12">
-                    <div class="row">
-                      <div class="input-field col s11 m11">
-                        <i class="material-icons prefix">confirmation_number</i>
-                        <input id="password" type="password" class="validate"></input>
-                        <label for="password">Enter password</label>
+                <div className="row">
+                  <form className="col s12">
+                    <div className="row">
+                      <div className="input-field col s11 m11">
+                        <i className="material-icons prefix">confirmation_number</i>
+                        <input onChange={this.passwordInput} id="password" type="password" className="validate"></input>
+                        <label htmlFor="password">Enter password</label>
                       </div>
                     </div>
                   </form>
                 </div>
               </div>
-              <div class="card-action">
-                <a href="#">search</a>
+              <div className="card-action">
+                <a onClick={this.getBreachedPassword} href="#">search</a>
               </div>
             </div>
           </div>
         </div>
 
-
-
       </div>
     );
+  }
+
+  passwordInput = (event) => {
+    this.setState({passwordInput: event.target.value});
+  }
+
+  getBreachedPassword = async () => {
+    this.setState({passwordNotFound: false, passwordOutput: ''})
+    let password = this.state.passwordInput;
+    const passwordHash = (await this.sha1Generator(password)).toUpperCase();
+    console.log('aaaaaaaa', passwordHash)
+    fetch(`http://localhost:4000/api/password/${passwordHash.slice(0,5)}`)
+    .then(res => res.json())
+    .then(res => {
+      const rows = res.split('\r\n');
+      const found = rows.find(val => val.split(':')[0] === passwordHash.slice(5));
+      if (found && found.split(':')[1] > 0) {
+        this.setState({passwordOutput: found.split(':')[1]})
+      }
+      else {
+        this.setState({passwordNotFound: true})
+      }
+    })
+    .catch(err => {
+      this.setState({passwordNotFound: true})
+      console.log(err)
+    });
+  }
+
+  sha1Generator = async (passwordString) => {
+    const result = [];
+    const buffer = new TextEncoder().encode(passwordString);
+    const hashBuffer = await crypto.subtle.digest('SHA-1', buffer)
+    const bytes = new DataView(hashBuffer)
+    for (let i=0;i<bytes.byteLength;i+=4) {
+      const hexValue = bytes.getUint32(i).toString(16);
+      result.push(hexValue);
+    }
+    return result.join('');
   }
 }
 
