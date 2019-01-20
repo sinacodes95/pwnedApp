775b4dcbd8bca180da6932dc97bceeac009fddc4
sina <sinakabki@gmail.com>
2019-01-20 00:06:45 +0000

    Password functionality implemented

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -4,32 +4,32 @@ import {NavLink} from 'react-router-dom';
 const Home = () => {
   return (
     <div className='container center-align'>
-    <div class="row">
-    <div class="col s6 l6">
-      <div class="card">
-        <div class="card-image">
+    <div className="row">
+    <div className="col s6 l6">
+      <div className="card">
+        <div className="card-image">
           <img alt='emailStockPhoto.jpg' src="emailStockPhoto.jpg"></img>
-          <span class="card-title">Email</span>
+          <span className="card-title">Email</span>
         </div>
-        <div class="card-content">
+        <div className="card-content">
           <p>Click below to check if you have an account that has been compromised in a data breach at any point. We will show you what we find.</p>
         </div>
-        <div class="card-action">
+        <div className="card-action">
           <NavLink to="/Email">click Here</NavLink>
         </div>
       </div>
     </div>
 
-    <div class="col s6 m6">
-      <div class="card">
-        <div class="card-image">
+    <div className="col s6 m6">
+      <div className="card">
+        <div className="card-image">
           <img alt='passwordStockPhoto.jpg' src="passwordStockPhoto.jpg"></img>
-          <span class="card-title">Password</span>
+          <span className="card-title">Password</span>
         </div>
-        <div class="card-content">
+        <div className="card-content">
           <p>Pwned Passwords are 551,509,767 real world passwords previously exposed in data breaches. Click below to see if your Password was one of them.</p>
         </div>
-        <div class="card-action">
+        <div className="card-action">
           <NavLink to="/Password">click here</NavLink>
         </div>
       </div>
--- web/src/containers/Password.js
+++ web/src/containers/Password.js
@@ -6,12 +6,12 @@ class Password extends Component {
     this.state = {
       passwordInput: '',
       passwordOutput: '',
-      isPasswordActive: false,
-      passwordNotFound: false
+      passwordNotFound: '2'
     }
   }
 
   render() {
+    const { passwordOutput, passwordNotFound } = this.state;
     return (
       <div className='container'>
         <div className="row">
@@ -39,37 +39,61 @@ class Password extends Component {
               </div>
             </div>
           </div>
+          {passwordOutput.length && passwordNotFound==='0' ?
+              <div className="col s12 m6">
+                <div className="card blue-grey darken-1">
+                  <div className="card-content white-text">
+                    <span className="card-title">Oh! Nooo!</span>
+                    <p>Your password was found in {passwordOutput} breaches! <i className="material-icons prefix">sentiment_very_dissatisfied</i></p>
+                  </div>
+                  <div className="card-action">
+                    <a href="#">close</a>
+                  </div>
+                </div>
+              </div>
+              :passwordNotFound==='1'?
+              <div className="col s12 m6">
+                <div className="card blue-grey darken-1">
+                  <div className="card-content white-text">
+                    <span className="card-title">Wohoo!</span>
+                    <p>Your password was not found in any breaches! <i className="material-icons prefix">sentiment_very_satisfied</i></p>
+                  </div>
+                  <div className="card-action">
+                    <a href="#">close</a>
+                  </div>
+                </div>
+              </div>
+            :null}
         </div>
-
       </div>
     );
   }
 
   passwordInput = (event) => {
-    this.setState({passwordInput: event.target.value});
+    this.setState({ passwordInput: event.target.value });
   }
 
   getBreachedPassword = async () => {
-    this.setState({passwordNotFound: false, passwordOutput: ''})
+    this.setState({ passwordNotFound: '2', passwordOutput: '' })
     let password = this.state.passwordInput;
     const passwordHash = (await this.sha1Generator(password)).toUpperCase();
-    console.log('aaaaaaaa', passwordHash)
-    fetch(`http://localhost:4000/api/password/${passwordHash.slice(0,5)}`)
-    .then(res => res.json())
-    .then(res => {
-      const rows = res.split('\r\n');
-      const found = rows.find(val => val.split(':')[0] === passwordHash.slice(5));
-      if (found && found.split(':')[1] > 0) {
-        this.setState({passwordOutput: found.split(':')[1]})
-      }
-      else {
-        this.setState({passwordNotFound: true})
-      }
-    })
-    .catch(err => {
-      this.setState({passwordNotFound: true})
-      console.log(err)
-    });
+    fetch(`http://localhost:4000/api/password/${passwordHash.slice(0, 5)}`)
+      .then(res => res.json())
+      .then(res => {
+        const rows = res.split('\r\n');
+        const found = rows.find(val => val.split(':')[0] === passwordHash.slice(5));
+        if (found && found.split(':')[1] > 0) {
+          this.setState({ passwordOutput: found.split(':')[1], passwordNotFound: '0' })
+        }
+        else {
+          this.setState({ passwordNotFound: '1' })
+        }
+      })
+      .catch(err => {
+        if (err) {
+          this.setState({ passwordNotFound: '1' })
+        }
+      });
   }
 
   sha1Generator = async (passwordString) => {
@@ -77,7 +101,7 @@ class Password extends Component {
     const buffer = new TextEncoder().encode(passwordString);
     const hashBuffer = await crypto.subtle.digest('SHA-1', buffer)
     const bytes = new DataView(hashBuffer)
-    for (let i=0;i<bytes.byteLength;i+=4) {
+    for (let i = 0; i < bytes.byteLength; i += 4) {
       const hexValue = bytes.getUint32(i).toString(16);
       result.push(hexValue);
     }
