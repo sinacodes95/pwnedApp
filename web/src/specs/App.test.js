a65ae1a5b84fe152d733fed755258c631a757727
sina <sinakabki@gmail.com>
2019-01-20 23:00:08 +0000

    __snapshots__ added to .gitignore

---
+++ web/src/specs/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../containers/App';
+import Enzyme, {shallow} from 'enzyme';
+
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('it shallow renders correctly', () => {
+  const wrapper = shallow(
+    <App />
+  );
+
+  expect(wrapper).toMatchSnapshot();
+});
