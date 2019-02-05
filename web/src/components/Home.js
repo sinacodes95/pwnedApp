522262d11c089b734254963a922a7897d27b5cae
sina <sinakabki@gmail.com>
2019-02-05 19:15:45 +0000

    image dependecncies removed

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -1,41 +1,35 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className='container center-align'>
-    <div className="row">
-    <div className="col s6 l6">
-      <div className="card">
-        <div className="card-image">
-          <img alt='emailStockPhoto.jpg' src="emailStockPhoto.jpg"></img>
-          <span className="card-title">Email</span>
+      <div className="row">
+        <div className="col s6 l6">
+          <div className="card">
+            <span className="card-title">Email</span>
+            <div className="card-content">
+              <p>Click below to check if you have an account that has been compromised in a data breach at any point. We will show you what we find.</p>
+            </div>
+            <div className="card-action">
+              <NavLink to="/Email">click Here</NavLink>
+            </div>
+          </div>
         </div>
-        <div className="card-content">
-          <p>Click below to check if you have an account that has been compromised in a data breach at any point. We will show you what we find.</p>
-        </div>
-        <div className="card-action">
-          <NavLink to="/Email">click Here</NavLink>
-        </div>
-      </div>
-    </div>
 
-    <div className="col s6 m6">
-      <div className="card">
-        <div className="card-image">
-          <img alt='passwordStockPhoto.jpg' src="passwordStockPhoto.jpg"></img>
-          <span className="card-title">Password</span>
-        </div>
-        <div className="card-content">
-          <p>Pwned Passwords are 551,509,767 real world passwords previously exposed in data breaches. Click below to see if your Password was one of them.</p>
-        </div>
-        <div className="card-action">
-          <NavLink to="/Password">click here</NavLink>
+        <div className="col s6 m6">
+          <div className="card">
+            <span className="card-title">Password</span>
+            <div className="card-content">
+              <p>Pwned Passwords are 551,509,767 real world passwords previously exposed in data breaches. Click below to see if your Password was one of them.</p>
+            </div>
+            <div className="card-action">
+              <NavLink to="/Password">click here</NavLink>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  </div>
   );
 }
 
