415b1bb27b23786e79c2f4993586955177eea8ad
sina <sinakabki@gmail.com>
2019-02-06 22:59:49 +0000

    images added

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -7,7 +7,10 @@ const Home = () => {
       <div className="row">
         <div className="col s6 l6">
           <div className="card">
-            <span className="card-title">Email</span>
+            <div class="card-image">
+              <img src="https://s3-eu-west-1.amazonaws.com/sinacodes/img/emailStockPhoto.jpg" alt="email"></img>
+              <span className="card-title">Email</span>
+            </div>
             <div className="card-content">
               <p>Click below to check if you have an account that has been compromised in a data breach at any point. We will show you what we find.</p>
             </div>
@@ -19,7 +22,10 @@ const Home = () => {
 
         <div className="col s6 m6">
           <div className="card">
-            <span className="card-title">Password</span>
+            <div>
+              <img src="https://s3-eu-west-1.amazonaws.com/sinacodes/img/passwordStockPhoto.jpg" alt="email"></img>
+              <span className="card-title">Password</span>
+            </div>
             <div className="card-content">
               <p>Pwned Passwords are 551,509,767 real world passwords previously exposed in data breaches. Click below to see if your Password was one of them.</p>
             </div>
